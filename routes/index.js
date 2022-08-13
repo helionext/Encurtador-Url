8384d22c031eed236778b3add0924015c4b9cf21
helionext <juniorjobs.zen@gmail.com>
2022-08-13 16:02:35 -0300

    redirecionando a url e atualizando estatisticas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var router = express.Router();
 const Link = require('../models/link');
+
+router.get('/:code/stats', async(req, res, next) => {
+  const code = req.params.code;
+
+  const resultado = await Link.findOne({where: {code}});
+
+  if(!resultado) return res.sendStatus(404);
+
+  res.render('stats', resultado.dataValues);
+})
+
+
+router.get('/:code', async (req, res, next) => {
+  const code = req.params.code;
+
+  const resultado = await Link.findOne({where: {code}});
+
+  if(!resultado) return res.sendStatus(404);
+
+  resultado.hits++;
+  await resultado.save();
+
+  res.redirect(resultado.url);
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encurtador' });
