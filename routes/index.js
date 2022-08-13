9dddd69012da80f662255309bbe28d79008e1ba5
helionext <juniorjobs.zen@gmail.com>
2022-08-13 14:44:57 -0300

    layout da pagina e algoritimo de geracao da URL encurtada

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Encurtador' });
 });
 
+function generateCode() {
+  let text = '';
+  const possible = 'ABCDEFGHIJLMNOPQRSTUVXZabcdefghijlmnopqrstuvxz0123456789';
+  for (let i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+
+  return text;
+}
+
+router.post('/new', (req, res, next) => {
+  const url = req.body.url;
+  const code = generateCode();
+  res.send('http://localhost/3000/' + code)
+})
 module.exports = router;
